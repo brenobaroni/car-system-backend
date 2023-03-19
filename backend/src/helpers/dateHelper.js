58abaeaafb8377d3a0a40094816142624b3f77e7
Breno Baroni <breno_baroni@hotmail.com>
2023-03-18 23:31:25 -0300

    change paste name

--- backend/src/helpers/dateHelper.js
+++
@@ -1,7 +0,0 @@
-const currentDateTime = () => {
-    return new Date().toISOString().slice(0, 19).replace('T', ' ');
-}
-
-module.exports = {
-    currentDateTime
-}
--- backend/src/server.js
+++
@@ -1,9 +0,0 @@
-const app = require('./app');
-
-//Config environment variables in .env file
-require('dotenv').config();
-
-const PORT = process.env.SERVER_PORT;
-
-app.listen(PORT, () => console.log(`Server running on port ${PORT}.`));
-
